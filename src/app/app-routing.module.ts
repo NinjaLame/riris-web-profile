89020fa7adf54fe13b816443c70ec5883233be4e
bayu <bayu.asrori@gmail.com>
2021-03-24 19:39:46 +0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WellcomePageComponent } from './wellcome-page/wellcome-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: WellcomePageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
